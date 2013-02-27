2dfe79ccac1107a38ac2a994c627f796f2bbcb27
Jonathan Hartley <tartley@tartley.com>
2013-02-27 17:39:32 +0000

    first working unit test based on imp stolen from  'classcalc' ply example

--- toml/tests/test_toml.py
+++ toml/tests/test_toml.py
@@ -5,6 +5,12 @@ from ..toml import loads
 
 class LoadsTest(unittest.TestCase):
 
-    def test_loads_empty_string(self):
-        self.assertEqual(loads(''), None)
+    #def test_t_empty(self):
+        #self.assertEqual(loads(''), {})
+
+    def test_p_statement_assignment(self):
+        self.assertEqual(loads('abc=123'), {'abc': 123})
+
+    #def test_t_comment(self):
+        #self.assertEqual(loads('# comment'), {})
 
--- toml/toml.py
+++ toml/toml.py
@@ -1,8 +1,68 @@
-from ply import yacc
+from ply import lex, yacc
+
+class TomlParser():
+
+    def __init__(self):
+        lex.lex(module=self)
+        yacc.yacc(module=self)
+
+    tokens = (
+        'NAME','NUMBER',
+    )
+
+    literals = ['=']
+
+    #t_ignore = " \t"
+
+    t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+    def t_NUMBER(self, token):
+        r'\d+'
+        token.value = int(token.value)
+        return token
+
+    #def t_newline(token):
+        #r'\n+'
+        #token.lexer.lineno += token.value.count("\n")
+
+    def t_error(self, token):
+        print("Illegal character '%s'" % token.value[0])
+        token.lexer.skip(1)
+
+    # ------- parsing rules
+
+    def p_statement_assign(self, p):
+        'statement : NAME "=" expression'
+        if p[0] is None:
+            p[0] = {}
+        p[0][p[1]] = p[3]
+
+    def p_expression_number(self, p):
+        "expression : NUMBER"
+        p[0] = p[1]
+
+    def p_error(p):
+        if p:
+            print("Syntax error at '%s'" % p.value)
+        else:
+            print("Syntax error at EOF")
 
 
-# Public outermost function,
-# only system tested, not unit tested
-def loads(string):
-    return yacc.parse(string)
+    # ------- public entry point
+
+    def parse(self, text):
+        return yacc.parse(text)
+
+
+def loads(text):
+    return TomlParser().parse(text)
+
+
+
+
+
+
+
+
+
 
