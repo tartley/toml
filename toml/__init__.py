86d7b6bcf6bd9b6ce8735cc9a765be9dce45cdb9
Jonathan Hartley <tartley@tartley.com>
2013-02-28 20:54:04 +0000

    Changes reqd for python3.3
    Switched from Python2.7, virtualenv[wrapper],
    to Python3.3 which comes with 'venv' built-in.
    For some reason this required changing an import
    in toml/__init__.py, not sure why.

--- toml/__init__.py
+++ toml/__init__.py
@@ -7,5 +7,5 @@ __version__ = '0.0.1dev'
 # __doc__, __name__ &__version__ are seen by pydoc, read by setup.py,
 # and generate output for --help.
 
-from toml import loads
+from .toml import loads
 
