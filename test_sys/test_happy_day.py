cbf77a6529c18cff1136fdb0f7261e3b241d1901
Jonathan Hartley <tartley@tartley.com>
2013-03-01 08:58:35 +0000

    Replace newline char in test data with backslash & 'n'.
    From spec: 'Strings are single line values'. Hence newline char in string
    (as was formerly in test data) is not legal. The intent is that a slash n
    in a string is unescaped into a newline in the output data.

--- test_sys/test_happy_day.py
+++ test_sys/test_happy_day.py
@@ -17,7 +17,7 @@ class HappyDayTest(unittest.TestCase):
             [owner]
             name = "Tom Preston-Werner"
             organization = "GitHub"
-            bio = "GitHub Cofounder & CEO\nLikes tater tots and beer."
+            bio = "GitHub Cofounder & CEO\\nLikes tater tots and beer."
             dob = 1979-05-27T07:32:00Z # First class dates? Why not?
 
             [database] # comment after a category
